4e98dd8f4a974b514f9ead6a05c13d8f5d5d0c03
Mykola <mykola@mail.com>
2024-01-15 19:53:21 +0200

    Slug converter for product

--- MySite/goods/views.py
+++ MySite/goods/views.py
@@ -10,11 +10,17 @@ def catalog(request):
     goods = Products.objects.all()
 
     context = {
-        'title': 'HOME - каталог товарів',
+        'title': 'HOME - Каталог',
         'goods': goods,
     }
     return render(request, "goods/catalog.html", context)
 
 
-def product(request):
-    return render(request, "goods/product.html")
+def product(request, product_slug):
+
+    product = Products.objects.get(slug=product_slug)
+
+    context = {
+        'product': product
+    }
+    return render(request, "goods/product.html", context=context)
