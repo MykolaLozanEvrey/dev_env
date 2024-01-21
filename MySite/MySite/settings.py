88fc9b897f33603ec81745bf9a1ec81944f666a3
Mykola <mykola@mail.com>
2024-01-21 23:32:57 +0200

    PostgreSQL data base connected & fuul text search

--- MySite/MySite/settings.py
+++ MySite/MySite/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.contrib.postgres",
 
     "debug_toolbar",
     
@@ -85,9 +86,13 @@ WSGI_APPLICATION = "MySite.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'home',
+        'USER': 'home',
+        'PASSWORD': '1221',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
