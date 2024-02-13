468c466571448c095e98af8892fc2efb55bac43d
Mykola <mykola@mail.com>
2024-02-13 14:40:49 +0200

    End on this time 2-bit

--- MySite/users/views.py
+++ MySite/users/views.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib import auth, messages
+from django.db.models import Prefetch
 from django.http import HttpResponseRedirect
 from django.shortcuts import redirect, render
 from django.urls import reverse
+from orders.models import Order, OrderItem
 
 from carts.models import Cart
 
@@ -78,7 +80,22 @@ def profile(request):
     else:
         form = ProfileForm(instance=request.user)
 
-    context = {"title": "Home - Кабинет", "form": form}
+    orders = (
+        Order.objects.filter(user=request.user)
+        .prefetch_related(
+            Prefetch(
+                "orderitem_set",
+                queryset=OrderItem.objects.select_related("product"),
+            )
+        )
+        .order_by("-id")
+    )
+
+    context = {
+        "title": "Home - Кабинет",
+        "form": form,
+        "orders": orders
+        }
     return render(request, "users/profile.html", context)
 
 def users_cart(request):
